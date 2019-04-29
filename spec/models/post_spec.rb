de4d9ade6f94dca7ab22f6abd73302bea8a73b11
juancu8 <juancu8@gmail.com>
2019-04-30 00:54:02 +0200

    Start models with TDD and validations

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+
+  describe "validations" do
+    it "validate presence of require fields" do
+      should validate_presence_of(:title)
+      should validate_presence_of(:content)
+      should validate_presence_of(:published)
+      should validate_presence_of(:user_id)
+    end
+
+    it "validate relations" do
+      should belong_to(:user)
+    end
+  end
+end
