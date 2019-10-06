b73b339915990cdd9a1303b52b5bea3ed8107c2e
juancu8 <juancu8@gmail.com>
2019-10-06 13:19:39 +0200

    Pass test

--- spec/requests/private_posts_spec.rb
+++ spec/requests/private_posts_spec.rb
@@ -14,26 +14,32 @@ RSpec.describe "Posts with authentication", type: :request do
     context "with valid auth" do
       context "when requisting other's author post" do
         context "when post is public" do
+
           before { get "/posts/#{other_user_post.id}", headers: auth_headers }
+
           context "payload" do
-            subject { JSON.parse(response.body) }
+            subject { payload }
             it {is_expected.to include(:id)}
           end
           context "response" do
             subject { response } 
             it {is_expected.to have_http_status(:ok)}
           end
+
         end
         context "when post is draft" do
+
           before { get "/posts/#{other_user_post_draft.id}", headers: auth_headers }
+
           context "payload" do
-            subject { JSON.parse(response.body) }
+            subject { payload }
             it {is_expected.to include(:error)}
           end
           context "response" do
             subject { response } 
             it {is_expected.to have_http_status(:not_found)}
           end
+
         end
       end
       context "when requisting user's post" do
@@ -47,10 +53,10 @@ RSpec.describe "Posts with authentication", type: :request do
   describe "PUT /posts" do
   end
   
-    # it "should return OK" do
-    #   get '/posts'
-    #   payload = JSON.parse(response.body)
-    #   expect(payload).to be_empty
-    #   expect(response).to have_http_status(200)
-    # end
+  private 
+
+  # Access [:id] & ['id']
+  def payload
+    JSON.parse(response.body).with_indifferent_access
+  end
 end
