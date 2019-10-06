cd6d9b549ad64cbe926a2b87500f7feed016f745
juancu8 <juancu8@gmail.com>
2019-10-06 13:02:07 +0200

    Add controller post auth method for current user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, only: [:create, :update]
 
   rescue_from  Exception do |e|
     #log.error"#{ e.message}"
@@ -46,5 +47,22 @@ class PostsController < ApplicationController
 
   def update_params
     params.require(:post).permit(:title, :content, :published)
+  end
+
+  def authenticate_user!
+    # Bearer xxxxx
+    token_regex = /Bearer {\w+}/
+    # Read auth HEADER
+    headers = request.headers
+    # Check if is valid 
+    if headers['Authorization'].present? && headers['Authorization'].match(token_regex)
+      token = headers['Authorization'].match(token_regex)[1]
+      #User have token
+      if ( Current.user = User.find_by_auth_token(token) )
+        return 
+      end
+    end
+    
+    render json: {error: 'Unauthorized'}, status: :unauthorized
   end
 end
